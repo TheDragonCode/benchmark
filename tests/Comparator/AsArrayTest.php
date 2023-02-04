f573cf61a69a05ac09cb70453a4e2a561ab7a6f1
Andrey Helldar <helldar@dragon-code.pro>
2023-02-04 16:41:34 +0300

    Updated tests

---
+++ tests/Comparator/AsArrayTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Comparator;
+
+use Tests\TestCase;
+
+class AsArrayTest extends TestCase
+{
+    public function testDefault(): void
+    {
+        $this->comparator()->compare([
+            'foo' => fn () => $this->work(),
+            'bar' => fn () => $this->work(),
+        ]);
+
+        $this->assertTrue(true);
+    }
+
+    public function testIterations(): void
+    {
+        $this->comparator()->iterations(5)->compare([
+            'foo' => fn () => $this->work(),
+            'bar' => fn () => $this->work(),
+        ]);
+
+        $this->assertTrue(true);
+    }
+
+    public function testWithoutData(): void
+    {
+        $this->comparator()->withoutData()->compare([
+            'foo' => fn () => $this->work(),
+            'bar' => fn () => $this->work(),
+        ]);
+
+        $this->assertTrue(true);
+    }
+
+    public function testRound(): void
+    {
+        $this->comparator()->round(2)->compare([
+            'foo' => fn () => $this->work(),
+            'bar' => fn () => $this->work(),
+        ]);
+
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Comparator/AsCallbackTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Comparator;
+
+use Tests\TestCase;
+
+class AsCallbackTest extends TestCase
+{
+    public function testDefault(): void
+    {
+        $this->comparator()->compare(
+            fn () => $this->work(),
+            fn () => $this->work(),
+        );
+
+        $this->assertTrue(true);
+    }
+
+    public function testIterations(): void
+    {
+        $this->comparator()->iterations(5)->compare(
+            fn () => $this->work(),
+            fn () => $this->work(),
+        );
+
+        $this->assertTrue(true);
+    }
+
+    public function testWithoutData(): void
+    {
+        $this->comparator()->withoutData()->compare(
+            fn () => $this->work(),
+            fn () => $this->work(),
+        );
+
+        $this->assertTrue(true);
+    }
+
+    public function testRound(): void
+    {
+        $this->comparator()->round(2)->compare(
+            fn () => $this->work(),
+            fn () => $this->work(),
+        );
+
+        $this->assertTrue(true);
+    }
+}
