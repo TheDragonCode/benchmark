d2da2b68f570fdea570dedfed5207b21e67cef68
Andrey Helldar <helldar@dragon-code.pro>
2023-03-02 19:14:10 +0300

    Update Memory.php

--- src/Services/Memory.php
+++ src/Services/Memory.php
@@ -14,7 +14,7 @@ class Memory
 
     public function now(): int
     {
-        return memory_get_usage(true);
+        return memory_get_peak_usage(true);
     }
 
     public function diff(int $memory): int
