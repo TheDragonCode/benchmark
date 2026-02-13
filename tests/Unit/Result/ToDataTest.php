4e323ab3686ae81c5b724a2a312d2a37888089dd
Andrey Helldar <helldar@dragon-code.pro>
2026-02-13 21:09:39 +0300

    [4.x] Console output added

--- tests/Unit/Result/ToDataTest.php
+++ tests/Unit/Result/ToDataTest.php
@@ -1,3 +1,87 @@
 <?php
 
 declare(strict_types=1);
+
+test('named array', function () {
+    $result = benchmark()
+        ->compare([
+            'foo' => static fn () => usleep(50),
+            'bar' => static fn () => true,
+        ])
+        ->toData();
+
+    expect($result['foo'])
+        ->min->time->toBeGreaterThan(5)
+        ->max->time->toBeGreaterThan(5)
+        ->avg->time->toBeGreaterThan(5)
+        ->total->time->toBeGreaterThan(5);
+
+    expect($result['bar'])
+        ->min->time->toBeLessThan(1)
+        ->max->time->toBeLessThan(1)
+        ->avg->time->toBeLessThan(1)
+        ->total->time->toBeLessThan(1);
+});
+
+test('array without names', function () {
+    $result = benchmark()
+        ->compare([
+            static fn () => usleep(50),
+            static fn () => true,
+        ])
+        ->toData();
+
+    expect($result[0])
+        ->min->time->toBeGreaterThan(5)
+        ->max->time->toBeGreaterThan(5)
+        ->avg->time->toBeGreaterThan(5)
+        ->total->time->toBeGreaterThan(5);
+
+    expect($result[1])
+        ->min->time->toBeLessThan(1)
+        ->max->time->toBeLessThan(1)
+        ->avg->time->toBeLessThan(1)
+        ->total->time->toBeLessThan(1);
+});
+
+test('named callbacks', function () {
+    $result = benchmark()
+        ->compare(
+            foo: static fn () => usleep(50),
+            bar: static fn () => true,
+        )
+        ->toData();
+
+    expect($result['foo'])
+        ->min->time->toBeGreaterThan(5)
+        ->max->time->toBeGreaterThan(5)
+        ->avg->time->toBeGreaterThan(5)
+        ->total->time->toBeGreaterThan(5);
+
+    expect($result['bar'])
+        ->min->time->toBeLessThan(1)
+        ->max->time->toBeLessThan(1)
+        ->avg->time->toBeLessThan(1)
+        ->total->time->toBeLessThan(1);
+});
+
+test('callbacks without names', function () {
+    $result = benchmark()
+        ->compare(
+            static fn () => usleep(50),
+            static fn () => true,
+        )
+        ->toData();
+
+    expect($result[0])
+        ->min->time->toBeGreaterThan(5)
+        ->max->time->toBeGreaterThan(5)
+        ->avg->time->toBeGreaterThan(5)
+        ->total->time->toBeGreaterThan(5);
+
+    expect($result[1])
+        ->min->time->toBeLessThan(1)
+        ->max->time->toBeLessThan(1)
+        ->avg->time->toBeLessThan(1)
+        ->total->time->toBeLessThan(1);
+});
