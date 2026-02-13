aec9d00f76a6c45865cb29149be1ea395afb0b72
Andrey Helldar <helldar@dragon-code.pro>
2026-02-13 14:24:40 +0300

    [4.x] Add `ResultData` and `MetricData` DTO classes

---
+++ src/Data/ResultData.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DragonCode\Benchmark\Data;
+
+readonly class ResultData
+{
+    public function __construct(
+        public MetricData $min,
+        public MetricData $max,
+        public MetricData $avg,
+        public MetricData $total,
+    ) {}
+}
