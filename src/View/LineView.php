60d39f0bb64b11dc4694ade8a9265ba4a84efd4e
Andrey Helldar <helldar@dragon-code.pro>
2026-02-14 19:53:11 +0300

    [4.x] Remove redundant `$streamName` property and default to `php://stderr` in `View` classes

--- src/View/LineView.php
+++ src/View/LineView.php
@@ -6,8 +6,6 @@ namespace DragonCode\Benchmark\View;
 
 class LineView extends View
 {
-    protected string $streamName = 'php://stdout';
-
     public function newLine(int $count = 1): void
     {
         for ($i = 0; $i < $count; $i++) {
