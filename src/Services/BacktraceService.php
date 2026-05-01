9c0d2af835a7cbce85dc787c00da3874b658d41f
Andrey Helldar <helldar@dragon-code.pro>
2026-05-01 21:17:57 +0300

    Improve `BacktraceService`: Add detection logic for class and method filtering in backtrace handling

--- src/Services/BacktraceService.php
+++ src/Services/BacktraceService.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace DragonCode\Benchmark\Services;
 
-use function array_reverse;
 use function debug_backtrace;
 use function getcwd;
+use function in_array;
 use function realpath;
-use function str_contains;
 use function str_replace;
 use function trim;
 
 class BacktraceService
 {
+    protected string $detectClass = AssertService::class;
+
+    protected array $detectMethods = [
+        'toBeRegressionTime',
+        'toBeRegressionMemory',
+    ];
+
     public function path(): string
     {
         return trim($this->resolve(), '\\/');
@@ -26,18 +32,21 @@ class BacktraceService
 
     protected function scriptPath(): string
     {
-        foreach (array_reverse($this->trace()) as $item) {
-            if (! $path = $item['file'] ?? false) {
+        foreach ($this->trace() as $item) {
+            $class  = $item['class']    ?? false;
+            $method = $item['function'] ?? false;
+            $file   = $item['file']     ?? false;
+            $line   = $item['line']     ?? 0;
+
+            if ($class !== $this->detectClass) {
                 continue;
             }
 
-            if (str_contains($path, 'vendor')) {
+            if (! in_array($method, $this->detectMethods, true)) {
                 continue;
             }
 
-            $line = $item['line'] ?? 0;
-
-            return $this->normalize($path) . '_' . $line;
+            return $this->normalize($file) . '_' . $line;
         }
 
         return 'unknown';
